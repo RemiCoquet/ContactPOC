c518d2bea017205d935d9e4ef6cc5537d72ccc2f
RemiCoquet <remi.coquet@gmail.com>
2019-07-16 14:28:57 +0200

    modify git ignore

---
+++ android/app/build/generated/source/buildConfig/release/com/contactpoc/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.contactpoc;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.contactpoc";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
